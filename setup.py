ba74cd42f2af72f5f9e0aa16f983f9f161c6b7f5
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2023-06-05 12:09:37 -0300

    Setup config changes

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 from distutils.core import setup
 
 setup(
-    name = 'simplekml',
+    name = 'simplekml2-for-python3',
     packages = ['simplekml'],
-    version = '1.2.8',
+    version = '1.0.0',
     description = 'A Simple KML creator',
-    author='Kyle Lancaster',
-    author_email='kyle.lan@gmail.com',
-    url='http://code.google.com/p/simplekml/',
+    author='Daniel Fernandez',
+    author_email='daf111@yahoo.com.ar',
+    url='https://github.com/daf111/simplekml2',
     license='GNU General Public License',
     classifiers=[
             'Development Status :: 5 - Production/Stable',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.0',
             'Programming Language :: Python :: 3.1',
@@ -29,6 +26,8 @@ setup(
           ],
     long_description="""
 simplekml is a python package which enables you to generate KML with as little effort as possible.
+
+Note: This project is a Fork from https://github.com/tjlang/simplekml but has support for Python 3.8 and superior.
 
 At the time of making this package nothing was available (at least I could not find anything) that could create KML files easily. You needed a lot of bloated code to even create a simple point. This is understandable because the KML standard is quite extensive, but what if you just work with the simple elements of KML like Document, Folder, Point, LineString and Polygon? This package supports those elements and everything documented in the KML Reference. With simplekml creating a KML file containing a point as simple as::
 
